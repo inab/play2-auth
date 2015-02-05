786c859874ebc846a497ba6d12e84e380f3d1637
José María Fernández González <jmfernandez@cnio.es>
2015-02-05 11:54:28 +0100

    Abstracting CookieSupport trait

---
+++ module/src/main/scala/jp/t2v/lab/play2/auth/AbstractAuthSupport.scala
@@ -0,0 +1,16 @@
+package jp.t2v.lab.play2.auth
+
+import play.api.libs.Crypto
+import play.api.mvc.{Result, Cookie}
+
+abstract trait AbstractAuthSupport { self: AuthConfig =>
+
+  def verifyHmac(cookie: Cookie): Option[String] = {
+    val (hmac, value) = cookie.value.splitAt(40)
+    if (Crypto.sign(value) == hmac) Some(value) else None
+  }
+
+  def bakeCookie(token: String)(result: Result): Result ;
+
+}
+
